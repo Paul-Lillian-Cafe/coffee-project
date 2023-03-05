cd93857cd10e3e405fdb6f7893f1eae5d2b67160
Paul Garcia <paul.ngarcia@yahoo.com>
2023-03-04 22:31:40 -0600

    sat night tinkering

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function renderCoffee(coffee) {
     var html = '<div id="cards" class="coffee col-6 col-sm-3 col-lg-3 card text-center ">';
     // html += '<td>' + coffee.id + '</td>';
     html += '<h3 class="card-header w-70 text-nowrap">' + coffee.name + '</h3>';
-    html += '<p class="p-1 card-body text-dark">' + coffee.roast + '</p>';
+    html += '<p id="cardstext" class="p-1 card-body text-dark">' + coffee.roast + '</p>';
     html += '</div>';
     return html;
 }
